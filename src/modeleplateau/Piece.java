d03ae8c19e9786482f527c63094e02f9346e190f
lucasheraut <Epulapp@EPULAPP92.univ-lyon1.fr>
2017-05-11 16:50:28 +0200

    Translation et rotation OP

--- src/modeleplateau/Piece.java
+++ src/modeleplateau/Piece.java
@@ -83,10 +83,6 @@ public class Piece {
 		coordx = 7-taille;
 	}
 	
-	public boolean rotation(Rotation r){
-		return true;
-	}
-	
 	public boolean translation(Translation t, Grille grille){
 		Case[][] grilleJeu = grille.getCases();
 		switch(t){
@@ -95,17 +91,18 @@ public class Piece {
 				int coordDroite;
 				for(int i = 0; i<taille;i++){
 					coordDroite = -1;
-					for(int j = 0; j<taille;j++){
-						if(cases[j][i] != null){
+					for(int j = taille-1; j>1;j--){
+						if(cases[j][i] != null && coordDroite != -1){
 							coordDroite = j;
 						}
 					}
 					if(coordDroite != -1){
-						if(coordDroite == grille.getNbColonnes()-1 || grilleJeu[coordDroite+1][i] != null){
+						if(coordDroite+coordx+1 == grille.getNbColonnes()-1 || grilleJeu[coordDroite+coordx+1][i+coordy] != null){
 							return false;
 						}
 					}
 				}
+				coordx++;
 				break;
 
 			case GAUCHE:
@@ -113,28 +110,93 @@ public class Piece {
 				int coordGauche;
 				for(int i = 0; i<taille;i++){
 					coordGauche = -1;
-					for(int j = taille-1; j>1;j--){
-						if(cases[j][i] != null){
+					for(int j = 0; j<taille;j++){
+						if(cases[j][i] != null && coordGauche != -1){
 							coordGauche = j;
 						}
 					}
 					if(coordGauche != -1){
-						if(coordGauche == 0 || grilleJeu[coordGauche-1][i] != null){
+						if(coordGauche+coordx == 0 || grilleJeu[coordGauche+coordx-1][i+coordy] != null){
 							return false;
 						}
 					}
 				}
+				coordx--;
 				break;
 
 			case BAS:
-			
+				//On regarde les cases les plus basses
+				int coordBas;
+				for(int i = 0; i<taille;i++){
+					coordBas = -1;
+					for(int j = taille-1; j>1;j--){
+						if(cases[i][j] != null && coordBas != -1){
+							coordBas = j;
+						}
+					}
+					if(coordBas != -1){
+						if(coordBas+coordy == grille.getNbLignes()-1 || grilleJeu[coordx+i][coordy+coordBas-1] != null){
+							return false;
+						}
+					}
+				}
+				coordy++;
 				break;
 		}
-		
-		
-		
 		return true;
 	}
+	
+	public boolean rotation(Rotation r, Grille grille){
+		Case[][] grilleJeu = grille.getCases();
+		switch (r){
+			case DROITE:
+				
+				for(int i=0; i<taille; i++){
+					for(int j=0; j<taille; j++){
+						if(cases[i][j] != null && grilleJeu[j+coordx][taille-1-i+coordy]!=null){
+							return false;
+						}
+					}
+				}
+				cases = doRotationDroite(cases);
+				break;
+				
+			case GAUCHE:
+				for(int i=0; i<taille; i++){
+					for(int j=0; j<taille; j++){
+						if(cases[i][j] != null && grilleJeu[taille-1-j+coordx][i+coordy]!=null){
+							return false;
+						}
+					}
+				}
+				cases = doRotationGauche(cases);
+				break;
+		}
+		return true;
+		
+	}
+	
+	public Case[][] doRotationDroite(Case[][] input){
 
+		int n =input.length;
+		int m = input[0].length;
+		Case [][] output = new Case [m][n];
 
+		for (int i=0; i<n; i++)
+			for (int j=0;j<m; j++)
+				output [j][n-1-i] = input[i][j];
+		return output;
+	}
+	
+	public Case[][] doRotationGauche(Case[][] input){
+
+		int n =input.length;
+		int m = input[0].length;
+		Case [][] output = new Case [m][n];
+
+		for (int i=0; i<n; i++)
+			for (int j=0;j<m; j++)
+				output [m-1-j][i] = input[i][j];
+		return output;
+	}
 }
